ed17f06247715aa50b32c39bee549216b01f7540
ronhill314 <ronhill314@gmail.com>
2023-04-11 15:41:55 -0500

    Add basic JS examples

--- app/index.js
+++ app/index.js
@@ -1,1 +1,49 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+const catalog = [
+  {
+    id: "1",
+    name: "shirt",
+    price: 4.0,
+    category: "clothing",
+  },
+  {
+    id: "2",
+    name: "burger",
+    price: 6.0,
+    category: "food",
+  },
+  {
+    id: "3",
+    name: "radio",
+    price: 10.0,
+    category: "electronics",
+  },
+  {
+    id: "4",
+    name: "steak",
+    price: 16.0,
+    category: "food",
+  },
+  {
+    id: "5",
+    name: "socks",
+    price: 7.0,
+    category: "clothing",
+  },
+];
+
+// const user = {
+//   username: "tyler3point0",
+//   cart: [],
+// };
+
+// console.log(catalog, user);
+
+console.log(catalog[2].price.toFixed(2));
+
+function add2Numbers(num1, num2) {
+  return num1 + num2;
+}
+
+const total = add2Numbers(1, 2);
+
+console.log(total);
